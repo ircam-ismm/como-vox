32ab7f16b9c76c8e73127fd750a9e0164920d854
Frederic Bevilacqua <frederic.bevilacqua@ircam.fr>
2021-01-15 14:47:22 +0100

    bug corrected

--- projects/default/scripts/data/beatTriggerFromGesturePeakAdapt.js
+++ projects/default/scripts/data/beatTriggerFromGesturePeakAdapt.js
@@ -4,25 +4,29 @@ function beatTriggerFromGestureMax(graph, helpers, outputFrame) {
   const secondsToBeats = conversion.secondsToBeats;
   const positionAddBeats = conversion.positionAddBeats;
 
-  const feedbackIntensity = 0.7; //for the intensity factor initllay set to 0.7
-  const gain = 1.0; // original gain  = 0.07 with acdeleramoeter / 9.81
-  const meanStdOrder = 5;
+  const feedbackFactor = 0.8; //for the intensity factor initllay set to 0.7
+  const gain = 1.; // original gain  = 0.07 with acdeleramoeter / 9.81
   const deltaOrder = 5;
-  const adaptThreshold = true
-  const meanThresholdAdapt =  1; // factor to multiply standar deviation
-  const meanThresholdMin =  50; // min threshold
-  const timeIntervalThreshold = 0.2; //  0.2 in seconds
-  const movingMeanStd = new helpers.algo.MovingMeanStd(meanStdOrder);
   const movingDelta = new helpers.algo.MovingDelta(deltaOrder);
+  const averageOrder = 5;
+  const movingAverage = new helpers.algo.MovingAverage(averageOrder);
+  
+  const meanThresholdAdapt =  0.5 // factor to multiply standar deviation
+  const meanThresholdMin = 4 // min threshold
+  const timeIntervalThreshold = 0.3; //  0.2 in seconds
+  const meanStdOrder = 5;
+  const movingMeanStd = new helpers.algo.MovingMeanStd(meanStdOrder);
 
   let lastBeatTime = null;
   let lastMean = +Infinity; // prevent kick on first frame
   let lastStd = 0; // prevent kick on first frame
-  let maxIntensity = 0; // max value after detection
   let previousIntensity = 0; //intensity at previous frame
-  let overMean = 0; // 1 if intensity > delta
+  let positiveDelta = 0; // 1 if intensity > delta
   let delta = 0;
+  let value = 0
   let memory = 0; // intensity[time-1] 
+  let oldDelta = -1;
+  let detection = 0;
 
   const parameters = {
     sensorsLatency: 1 / 60, // 60 Hz?
@@ -48,13 +52,16 @@ function beatTriggerFromGestureMax(graph, helpers, outputFrame) {
 
 
       // computing intensity using only one axis
-      let intensity = inputData['accelerationIncludingGravity'].x;
-      let value = Math.abs(movingDelta.process(intensity, inputData.metas.period));
-      value = value + feedbackIntensity * memory; // store value for next pass
+      const acceleration = inputData['accelerationIncludingGravity'].x;
+      let derivate = movingDelta.process(acceleration, inputData.metas.period);
+      value = Math.abs(derivate, 0) + feedbackFactor * memory; // store value for next pass
       memory = value;
-      intensity = value * gain;
+      let intensity = value * gain;
+      let intensityFiltered = movingAverage.process(intensity);
+      
+      //console.log(now);
 
-      delta = intensity - lastMean - (lastStd*meanThresholdAdapt + meanThresholdMin)
+      delta = intensityFiltered - lastMean - lastStd*meanThresholdAdapt - meanThresholdMin
 
       // @TODO should compensate latency depending on algorithm
       const time = now - parameters.sensorsLatency;
@@ -69,20 +76,43 @@ function beatTriggerFromGestureMax(graph, helpers, outputFrame) {
         mean: lastMean,
       };
 
-      if (overMean === 0) {
+      // not working
+      // if (detection === 0) {
+      //   if (Math.sign(delta) > 0 && Math.sign(oldDelta) < 0) {
+      //     detection = 1;
+      //     previousIntensity = intensityFiltered;
+      //     //beat.trigger = 1;
+      //   }  
+      // } else {
+      //   if (intensityFiltered > previousIntensity) {
+      //     previousIntensity = intensityFiltered;
+      //   } else {
+      //     if (intensityFiltered < previousIntensity && (now - lastBeatTime) > timeIntervalThreshold) {
+      //       beat.trigger = 1;
+      //       beat.intensity = intensityFiltered;
+      //       detection = 0;
+      //       previousIntensity = 0;
+      //       lastBeatTime = now;
+      //     }
+      //   }
+      // }
+
+      //console.log(intensity);
+
+      if (positiveDelta === 0) {
         if (delta > 0 && lastBeatTime === null) {
-          overMean = 1;
-          previousIntensity = intensity;
+          positiveDelta = 1;
+          previousIntensity = intensityFiltered;
         }
       } else {
-        if (intensity > previousIntensity) {
-          previousIntensity = intensity;
+        if (intensityFiltered > previousIntensity) {
+          previousIntensity = intensityFiltered;
         } else {
           lastBeatTime = now;
           beat.trigger = 1;
-          maxIntensity = previousIntensity;
+          beat.intensity = intensityFiltered;
           previousIntensity = 0;
-          overMean = 0;
+          positiveDelta = 0;
         }
       }
 
@@ -91,8 +121,8 @@ function beatTriggerFromGestureMax(graph, helpers, outputFrame) {
         lastBeatTime = null;
       }
 
-      [lastMean, lastStd] = movingMeanStd.process(intensity);
-
+      [lastMean, lastStd] = movingMeanStd.process(intensityFiltered);
+      oldDelta = delta;
       outputData['beat'] = beat;
       return outputFrame;
     },

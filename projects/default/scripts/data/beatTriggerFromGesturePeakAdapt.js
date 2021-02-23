b73a579428e4a08c1dbdcc7652cf453a0d271a14
Jean-Philippe Lambert <Jean-Philippe.Lambert@ircam.fr>
2021-02-23 17:56:46 +0100

    Graph: beatTriggerFromGesturePeakAdapt: even lower meanThreshold

--- projects/default/scripts/data/beatTriggerFromGesturePeakAdapt.js
+++ projects/default/scripts/data/beatTriggerFromGesturePeakAdapt.js
@@ -11,7 +11,7 @@ function beatTriggerFromGesturePeakAdapt(graph, helpers, outputFrame) {
   const averageOrder = 2;
   const movingAverage = new helpers.algo.MovingAverage(averageOrder);
   const meanThresholdAdapt =  1.; // factor to multiply standar deviation //1
-  const meanThresholdMin = 20; // min threshold 5
+  const meanThresholdMin = 5; // min threshold 5
   let inhibition = {
     min: 0.2, // seconds
     max: 0.5, // seconds
